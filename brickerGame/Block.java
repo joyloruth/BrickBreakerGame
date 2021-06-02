2024434961feeeb4cb3ed578dfb7eb0ae6f44606
Joy LoRuth <joyloruth@gmail.com>
2021-06-02 10:35:37 -0500

    brick breaker game

--- brickerGame/Block.java
+++ brickerGame/Block.java
@@ -1,5 +1,31 @@
 package brickerGame;
 
-public class Block {
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+
+public class Block extends Rectangle{
+	
+	Image pic;
+	boolean destroyed;
+	
+	int posX, posY;
+	int width, height;
+	
+	Block(int x, int y, int w, int h, String s){
+		
+		this.posX = x;
+		this.posY = y;
+		this.height = h;
+		this.width = w;
+		
+	}
+	
+	public void draw(Graphics g, Component c){
+		if(!destroyed)
+			g.drawImage(pic, x, y, w, h, c);
+		
+	}
 
 }
--- brickerGame/GamePanel.java
+++ brickerGame/GamePanel.java
@@ -1,5 +1,31 @@
 package brickerGame;
 
-public class GamePanel {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements KeyListener{
+	
+	
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
